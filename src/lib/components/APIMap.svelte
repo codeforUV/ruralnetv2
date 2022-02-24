5495dcc2f1f56471b24a55c852f0620cc0428bc3
Colby Hemond <hemond.colby@gmail.com>
2022-02-24 18:07:42 -0500

    add colored data points

--- src/lib/components/APIMap.svelte
+++ src/lib/components/APIMap.svelte
@@ -6,18 +6,44 @@
 
     onMount(async () => {
         if(browser) {
+
+            const getIcon = (speed) => {
+                const speedIcon = leaflet.icon({
+                        iconUrl: `https://img.icons8.com/material/24/${determineColorBySpeed(speed)}/filled-circle.png`,
+                    })
+                return speedIcon;
+            }
+
+            const determineColorBySpeed = (speed) => {
+                if (speed < 5) {
+                    return 'ff0000' //Red
+                } else if (speed >= 5 && speed < 100) {
+                    return 'FFFF00' //Yellow
+                } else if (speed > 100) {
+                    return '00FF00' //Green
+                } else {
+                    return '0000FF' //Blue
+                }
+            }
+
             const leaflet = await import('leaflet');
 
             const map = leaflet.map('map').setView([43.827, -72.295], 10);
-            // -72.305, 43.827
+            
+            
 
             leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            // leaflet.marker([51.5, -0.09]).addTo(map)
-            //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            //     .openPopup();
+            if (data) {
+                data.map((speedTest) => {
+                    if (speedTest.latitude && speedTest.longitude) {
+                        leaflet.marker([speedTest.latitude, speedTest.longitude], {icon: getIcon(speedTest.downloadSpeed)}).addTo(map)
+                    }
+                })
+            }
+            
         }
     });
 </script>
