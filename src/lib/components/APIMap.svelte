0cc94bc708904a4dca1d2ea41ef4f54626c3570e
Colby Hemond <hemond.colby@gmail.com>
2022-02-24 16:54:36 -0500

    add mapbox for svelte

--- src/lib/components/APIMap.svelte
+++ src/lib/components/APIMap.svelte
@@ -1,95 +1,38 @@
 <script>
-    import esriConfig from '@arcgis/core/config'
-    import Map from '@arcgis/core/Map'
-    import MapView from '@arcgis/core/views/MapView'
-    import Graphic from '@arcgis/core/Graphic'
-    import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer'
-    import Locate from '@arcgis/core/widgets/Locate'
+  import { Map, Geocoder, Marker, controls } from '@beyonk/svelte-mapbox'
 
-    export let data = [];
+    let mapComponent
+    let mapboxToken = 'pk.eyJ1IjoiY29sYnloZW1vbmQiLCJhIjoiY2wwMHk3c2ViMGxyNDNpbzAxbHhudzd0cSJ9.jy6YERoPDj1_F4vuYLY9xw'
+  
+  const { GeolocateControl, NavigationControl, ScaleControl } = controls
 
-    //This API Key is read only, @colbyhemond has set up restrictions around it as to what services are allowed and Referrers
-    //TODO: Create a Code for UV account on Arc GIS so our team can have access to manage and rotate the API Key
-    esriConfig.apiKey = 'AAPK234d2cbbeb2b4e0ba81514087b69c0b5M7sB-axZsH1fETdkNbUMLr2FYob-swNZHohVKx3-v3e6TMN_dTV0HJRcoge2QILS'
+  // Usage of methods like setCenter and flyto
+//   mapComponent.setCenter([lng,lat],zoom) // zoom is optional
+//   mapComponent.flyTo({center:[lng,lat]}) // documentation (https://docs.mapbox.com/mapbox-gl-js/example/flyto)
 
-    const createMap = (domNode) => {
-        const map = new Map({
-            basemap: "arcgis-topographic" // Basemap layer service
-        });
-
-        const view = new MapView({
-            map: map,
-            center: [-72.305, 43.827],
-            zoom: 9, 
-            container: domNode
-        });
-
-        const locate = new Locate({
-            view: view,
-            useHeadingEnabled: false,
-            goToOverride: function (view, options) {
-                options.target.scale = 1500;
-                return view.goTo(options.target);
-            }
-        });
-
-        view.ui.add(locate, "top-left");
-
-        const graphicsLayer = new GraphicsLayer();
-        map.add(graphicsLayer);
-        
-        const buildPointGraphic = (data) => {
-            const pointGraphic = new Graphic({
-                geometry: buildPoint(data.longitude, data.latitude),
-                symbol: buildMarkerSymbol(data.downloadSpeed)
-            });
-            graphicsLayer.add(pointGraphic);
-        }
-
-        const determineMarkerColor = (downloadSpeed) => {
-            if (downloadSpeed < 5) {
-                return 'red'
-            } else if (downloadSpeed >= 5 && downloadSpeed < 100) {
-                return 'yellow'
-            } else if (downloadSpeed > 100) {
-                return 'green'
-            } else {
-                return 'blue'
-            }
-        }
-
-        const buildMarkerSymbol = (downloadSpeed) => {
-            return {
-                type: "simple-marker",
-                color: determineMarkerColor(downloadSpeed),
-                outline: {
-                    color: [255, 255, 255],
-                    width: 1
-                }
-            }
-        }
-
-        const buildPoint = (longitude, latitude) => {
-            return {
-                type: "point",
-                longitude: longitude,
-                latitude: latitude
-            }
-        }
-
-        if (data) {
-            data.map((document) => {
-                if (document.downloadSpeed > 0 && document.longitude !== null) {
-                    buildPointGraphic(document)
-                }
-            })
-        }
-
-    };
-
+  // Define this to handle `eventname` events - see [GeoLocate Events](https://docs.mapbox.com/mapbox-gl-js/api/markers/#geolocatecontrol-events)
+  function eventHandler (e) {
+    const data = e.detail
+    // do something with `data`, it's the result returned from the mapbox event
+  }
 </script>
 
 <style>
+    :global(.mapboxgl-map) {
+        height: 100vh;
+        width: 100vw;
+    }
 </style>
 
-<div id="view" class="w-full h-full" use:createMap></div>
+<Geocoder value="(Near London)" accessToken={mapboxToken} on:result={placeChanged} on:clear={() => mapComponent.setCenter({ lng: 0, lat: 0 })} />
+<Map
+  accessToken={mapboxToken}
+  bind:this={mapComponent} 
+  on:recentre={e => console.log(e.detail.center.lat, e.detail.center.lng) } 
+  options={{ scrollZoom: false }} 
+>
+  <Marker lat={someLat} lng={someLng} color="rgb(255,255,255)" label="some marker label" popupClassName="class-name" /> // built in Marker component
+  <NavigationControl />
+  <GeolocateControl options={{ some: 'control-option' }} on:eventname={eventHandler} />
+  <ScaleControl />
+</Map>
