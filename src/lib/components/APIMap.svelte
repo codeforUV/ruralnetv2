265e625744ae5646f5fcf2e2c3bf6c7a23508962
Colby Hemond <hemond.colby@gmail.com>
2022-02-24 17:03:59 -0500

    add leaflet

--- src/lib/components/APIMap.svelte
+++ src/lib/components/APIMap.svelte
@@ -1,38 +1,32 @@
 <script>
-  import { Map, Geocoder, Marker, controls } from '@beyonk/svelte-mapbox'
+    import { onMount } from 'svelte';
+    import { browser } from '$app/env';
 
-    let mapComponent
-    let mapboxToken = 'pk.eyJ1IjoiY29sYnloZW1vbmQiLCJhIjoiY2wwMHk3c2ViMGxyNDNpbzAxbHhudzd0cSJ9.jy6YERoPDj1_F4vuYLY9xw'
-  
-  const { GeolocateControl, NavigationControl, ScaleControl } = controls
+    onMount(async () => {
+        if(browser) {
+            const leaflet = await import('leaflet');
 
-  // Usage of methods like setCenter and flyto
-//   mapComponent.setCenter([lng,lat],zoom) // zoom is optional
-//   mapComponent.flyTo({center:[lng,lat]}) // documentation (https://docs.mapbox.com/mapbox-gl-js/example/flyto)
+            const map = leaflet.map('map').setView([51.505, -0.09], 13);
 
-  // Define this to handle `eventname` events - see [GeoLocate Events](https://docs.mapbox.com/mapbox-gl-js/api/markers/#geolocatecontrol-events)
-  function eventHandler (e) {
-    const data = e.detail
-    // do something with `data`, it's the result returned from the mapbox event
-  }
+            leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            leaflet.marker([51.5, -0.09]).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+        }
+    });
 </script>
 
+
+<main>
+    <div id="map"></div>
+</main>
+
 <style>
-    :global(.mapboxgl-map) {
-        height: 100vh;
-        width: 100vw;
+    @import 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css';
+    main #map {
+        height: 800px;
     }
 </style>
-
-<Geocoder value="(Near London)" accessToken={mapboxToken} on:result={placeChanged} on:clear={() => mapComponent.setCenter({ lng: 0, lat: 0 })} />
-<Map
-  accessToken={mapboxToken}
-  bind:this={mapComponent} 
-  on:recentre={e => console.log(e.detail.center.lat, e.detail.center.lng) } 
-  options={{ scrollZoom: false }} 
->
-  <Marker lat={someLat} lng={someLng} color="rgb(255,255,255)" label="some marker label" popupClassName="class-name" /> // built in Marker component
-  <NavigationControl />
-  <GeolocateControl options={{ some: 'control-option' }} on:eventname={eventHandler} />
-  <ScaleControl />
-</Map>
