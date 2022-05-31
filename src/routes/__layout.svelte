80e45fcc711ca6f208a2ec510f50eb450c2d088f
Colby Hemond <hemond.colby@gmail.com>
2022-05-31 06:14:24 -0400

    add cookiewidget

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,17 @@
 <script>
+  
+  import CookiesWidget from "$lib/components/CookiesWidget.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import Header from "$lib/components/Header.svelte";
   import "../app.css";
+
 </script>
 
 <Header />
-<main>
+<main class='h-[100vh]'>
   <slot />
 </main>
-
+<CookiesWidget/>
 <Footer />
 
 <style>
