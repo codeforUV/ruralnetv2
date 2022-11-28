ad3b4d8f5ddf018a0a09d1fc6b6ab39026555bb9
jmslynn <72352820+jmslynn@users.noreply.github.com>
2022-11-28 12:29:44 -0500

    added draft of map legend

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -3,7 +3,6 @@
   import ApiMap from "$lib/components/APIMap.svelte";
 
   export let data = [];
-
 </script>
 
 <svelte:head>
@@ -13,13 +12,25 @@
 <div class="container mx-auto p-8 prose">
   <h1 class="text-4xl mb-6">Test Results</h1>
 </div>
-
-
-
-<div class='flex justify-center'>
-  <div class='w-full lg:w-1/2 lg:hover:transition-width lg:hover:w-full duration-300' >
-  <div class='p-4 w-full h-[75vh] lg:w-full ' id='mapContainer'>
-    <ApiMap {data} />
+<div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Map Legend</h2>
+    <p>
+      <span class="btn btn-circle btn-small no-animation">.</span> less than 25
+      mbps
+      <span>i</span>
+    </p>
+    <p><span>dot</span> 25 mbps to 100 mbps <span>i</span></p>
+    <p><span>dot</span> greater than 100 mbps <span>i</span></p>
   </div>
 </div>
+
+<div class="flex justify-center">
+  <div
+    class="w-full lg:w-1/2 lg:hover:transition-width lg:hover:w-full duration-300"
+  >
+    <div class="p-4 w-full h-[75vh] lg:w-full " id="mapContainer">
+      <ApiMap {data} />
+    </div>
+  </div>
 </div>
